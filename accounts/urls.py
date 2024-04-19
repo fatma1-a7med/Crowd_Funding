411084351c08eaac4fb36b148068f254c5f5c5f1
fatma1-a7med <fatmaahmed110099@gmail.com>
2024-04-20 01:14:54 +0200

    reset password by sending reset link to email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from accounts.views import  profile_view, create_user,update_profile,delete_account, create_user,index,activate,logout_view
+from django.contrib.auth import views as auth_views 
+
 urlpatterns = [
     path('profile/', profile_view , name='account.profile'),
     path('profile/update/', update_profile, name='account.profile.update'),
@@ -8,4 +10,10 @@ urlpatterns = [
     path('index/',index, name='index' ),
     path('activate/<str:uidb64>/<str:token>/',activate, name='activate' ),
     path('logged_out/',logout_view, name='logout' ),
+
+    #########
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
